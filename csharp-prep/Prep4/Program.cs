2d084fa99787fd48ac5ad554bbd055562816f720
HarrisonParrish <114323318+HarrisonParrish@users.noreply.github.com>
2023-06-10 22:10:19 -0700

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a series of numbers (enter 0 to stop):");
+
+        int input;
+        do
+        {
+            Console.Write("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+            int max = numbers.Max();
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Average: {average}");
+            Console.WriteLine($"Maximum: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers entered.");
+        }
     }
 }
