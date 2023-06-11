43c8f6e545f5f0684a284074b5547907310d302d
HarrisonParrish <114323318+HarrisonParrish@users.noreply.github.com>
2023-06-10 22:09:02 -0700

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        float gradePercentage = float.Parse(Console.ReadLine());
+
+
+        char letter;
+
+        if (gradePercentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        bool passing = gradePercentage >= 70;
+
+        if (passing)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep up the good work! Try again next time.");
+        }
+
+        // Stretch Challenge
+        int lastDigit = (int)(gradePercentage % 10);
+        char sign = ' ';
+
+        if (letter == 'A' && lastDigit >= 7)
+        {
+            sign = '+';
+        }
+        else if (letter != 'F')
+        {
+            if (lastDigit < 3)
+            {
+                sign = '-';
+            }
+        }
+
+        if (letter == 'A' && sign == '+')
+        {
+            Console.WriteLine("Grade: A+");
+        }
+        else if (letter == 'A' && sign == ' ')
+        {
+            Console.WriteLine("Grade: A");
+        }
+        else if (letter == 'A')
+        {
+            Console.WriteLine("Grade: A-");
+        }
+        else
+        {
+            Console.WriteLine($"Grade: {letter}{sign}");
+        }
     }
 }
